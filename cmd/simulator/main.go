1a33396694bf4499a8987c1c2de2ddad740ec7fe
gwuah <griffithawuah15@gmail.com>
2024-11-09 01:31:34 +0100

    feat: add simulator to test proxy

--- cmd/simulator/main.go
+++ cmd/simulator/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
 
+func main() {
+	proxyURL, _ := url.Parse("http://localhost:7777")
+	client := &http.Client{
+		Transport: &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
+		},
+	}
+
+	reqBody := bytes.NewBuffer([]byte(`{"key":"value"}`))
+
+	resp, err := client.Post("https://echo.free.beeceptor.com", "application/json", reqBody)
+	if err != nil {
+		panic(err)
+	}
+
+	resBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(resBody))
 }
